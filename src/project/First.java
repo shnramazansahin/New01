b399607f98adacd363ba4edfa5ec21f627065595
Ramazan <shnramazansahin@gmail.com>
2020-10-28 17:43:32 -0500

    first commit

--- src/project/First.java
+++ src/project/First.java
@@ -3,8 +3,19 @@ package project;
 public class First {
 
 	public static void main(String[] args) {
-		System.out.println("secon commit");
-
+		String st= "aaaabbbccdda";
+		String str= "";
+		int count=1;
+		for(int i=0;i<st.length()-1;i++) {
+			if(st.charAt(i)==st.charAt(i+1)) {
+				count++;
+			}else {
+				str= str+st.charAt(i)+ count;
+				count=1;
+				
+			}
+		}
+		System.out.println(str+ (st.charAt(st.length()-1)));
 	}
 
 }
